44a0a006bdc805d32b9a6c55e40d6b8a03f4d859
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-06-12 18:51:19 +0200

    Update main.pseudo.py

--- main.pseudo.py
+++ main.pseudo.py
@@ -5,7 +5,7 @@
 
 ## Try:
   ## telegram_db = new Database("telebot.db")
-  ## telebot = new Telebot(telegram_db.to_telebot_constructor)
+  ## telebot = new Telebot(telegram_db.to_telebot_constructor, fallback_error_telegram_id = 123)
   ## game = Game.setupTelebot(telebot)
   ## game_db = new Database("game.db")
   ## game.feedDataAndRun(game_db)

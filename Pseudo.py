bfa9700cdc49ba5f005648fba05ea72233fb620f
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-06-12 18:40:28 +0200

    Update and rename Pseudo.py to main.pseudo.py

--- Pseudo.py
+++
@@ -1,7 +0,0 @@
-## Crash_retries = 5
-## start sql server
-## run Telegram Server and load database, then 
-## allow players to connect
-
-## if server crashes, send admin a message
-## retries = 5: dont restart

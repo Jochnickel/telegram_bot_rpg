4a554559feb03299a4610946b26f3535481f4de0
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-06-12 17:51:48 +0200

    Create Pseudo.py

---
+++ Pseudo.py
@@ -0,0 +1,7 @@
+## Crash_retries = 5
+## start sql server
+## run Telegram Server and load database, then 
+## allow players to connect
+
+## if server crashes, send admin a message
+## retries = 5: dont restart

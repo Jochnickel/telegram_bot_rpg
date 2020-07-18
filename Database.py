15dd9bf863b397481583738c05940decffd2d92c
Your Name <you@example.com>
2020-07-18 16:53:31 +0000

    asd

--- Database.py
+++ Database.py
@@ -12,43 +12,48 @@ class Database:
 		global connection
 		return connection
 
-	def selectWhereUnsafe(self, cols, tableName, where_condition):
+	def selectWhereUnsafe(self, cols: str, tableName: str,
+						  where_condition: str) -> list:
 		cursor = connection.cursor()
 		cursor.fetchall()
 		cursor.execute("SELECT %s FROM %s WHERE %s" %
 					   (cols, tableName, where_condition))
 		return cursor.fetchall()
 
-	def selectWhereLessUnsafe(self, cols, tableName, where_col, where_val):
+	def selectWhereLessUnsafe(self, cols: str, tableName: str, where_col: str,
+							  where_val: str) -> list:
 		cursor = connection.cursor()
 		cursor.fetchall()
-		cursor.execute("SELECT %s FROM %s WHERE %s=?" %
-					   (cols, tableName, where_col), (where_val,))
+		cursor.execute(
+			"SELECT %s FROM %s WHERE %s=?" % (cols, tableName, where_col),
+			(where_val, ))
 		return cursor.fetchall()
 
-	def selectUnsafe(self, cols, tableName):
+	def selectUnsafe(self, cols: str, tableName: str) -> list:
 		cursor = connection.cursor()
 		cursor.fetchall()
 		cursor.execute("SELECT %s FROM %s" % (cols, tableName))
 		return cursor.fetchall()
 
-	def deleteFromUnsafe(self, tableName):
+	def deleteFromUnsafe(self, tableName: str) -> None:
 		cursor = connection.cursor()
 		cursor.fetchall()
 		cursor.execute("DELETE FROM %s" % tableName)
 		connection.commit()
 
-	def createTableIfNotExistsUnsafe(self, tableName, colsWOBrackets):
+	def createTableIfNotExistsUnsafe(self, tableName, columns) -> None:
 		cursor = connection.cursor()
 		cursor.fetchall()
 		cursor.execute("CREATE TABLE IF NOT EXISTS %s (%s)" %
-					   (tableName, colsWOBrackets))
+					   (tableName, columns))
 		connection.commit()
 
-	def createLinkedListIfNotExistsUnsafe(self, tableName, colNames):
-		return self.createTableIfNotExistsUnsafe(tableName, "ll_id INTEGER PRIMARY KEY, next_ll_id INTEGER, %s" % (colNames))
+	def createLinkedListIfNotExistsUnsafe(self, tableName, columns):
+		return self.createTableIfNotExistsUnsafe(
+			tableName,
+			"ll_id INTEGER PRIMARY KEY, next_ll_id INTEGER, %s" % (columns))
 
-	def insertLinkedListUnsafe(self, tableName, colNames, values):
+	def insertUnsafe(self, tableName, colNames, values) -> int:
 		cursor = connection.cursor()
 		cursor.fetchall()
 		cursor.execute("INSERT INTO %s (%s) VALUES (%s)" %
@@ -56,12 +61,28 @@ class Database:
 		connection.commit()
 		return cursor.lastrowid
 
-	def appendLinkedListUnsafe(self, tableName, currentList, values):
+	def insertLinkedListUnsafe(self, tableName, colNames, values) -> int:
 		cursor = connection.cursor()
 		cursor.fetchall()
-		iterator = self.selectWhereUnsafe(
-			"ll_id, next_ll_id", tableName, "ll_id=%s" % (currentList))
-		if (None == iterator or None == iterator[0]):
-			raise ValueError("currentList not found")
-		while(iterator):
+		cursor.execute("INSERT INTO %s (%s) VALUES (%s)" %
+					   (tableName, colNames, values))
+		connection.commit()
+		return cursor.lastrowid
+
+	def appendLinkedListUnsafe(self, tableName: str, colNames: str,
+							   values: str, currentList: int):
+		iterator = currentList
+		iteratorNext = self.selectNextLinkedListUnsafe(tableName, currentList)
+		while(iteratorNext):
+			iterator = iteratorNext
+			iteratorNext = self.selectNextLinkedListUnsafe(tableName, currentList)
+
+		while (iterator):
 			pass
+
+	def selectNextLinkedListUnsafe(self, tableName: str, currentItem: int) -> int | None:
+		item = self.selectWhereLessUnsafe("ll_id, next_ll_id", tableName,
+											"ll_id", currentItem)
+		if (None == item):
+			raise ValueError("Item not found")
+		return item[1]
--- Database_test.py
+++ Database_test.py
@@ -1,2 +1,68 @@
+import unittest
+
 from Database import Database
-asd = Database()
+
+
+class asd(unittest.TestCase):
+    def test_connection(self):
+        database = Database("test.db")
+        self.assertIsNotNone(database.getConnection())
+
+    def test_createTable(self):
+        database = Database("test.db")
+        database.deleteFromUnsafe("tab")
+        database.createTableIfNotExistsUnsafe(
+            "tab", "keyy INTEGER PRIMARY KEY, col1 TEXT")
+
+        tab1 = database.selectUnsafe("*", "tab")
+        self.assertEqual(tab1, [])
+
+        ins1 = database.insertUnsafe("tab", "col1", "'somevalue'")
+        self.assertIsNotNone(ins1)
+        self.assertIsInstance(ins1, int)
+
+        sel = database.selectUnsafe("*", "tab")
+        self.assertEqual(sel, [(ins1, 'somevalue')])
+        
+        sel = database.selectWhereUnsafe("*", "tab","col1='somevalue'")
+        self.assertEqual(sel, [(ins1, 'somevalue')])
+
+        sel = database.selectWhereUnsafe("col1", "tab","col1='somevalue'")
+        self.assertEqual(sel, [('somevalue',)])
+
+        
+        ins2 = database.insertUnsafe("tab", "col1", "'othervalue'")
+        self.assertIsNotNone(ins2)
+        self.assertIsInstance(ins2, int)
+
+        
+        sel = database.selectUnsafe("*", "tab")
+        self.assertEqual(sel, [(ins1, 'somevalue'),(ins2, 'othervalue')])
+        
+        sel = database.selectWhereUnsafe("*", "tab","col1='somevalue'")
+        self.assertEqual(sel, [(ins1, 'somevalue')])
+        
+        sel = database.selectWhereUnsafe("*", "tab","col1='othervalue'")
+        self.assertEqual(sel, [(ins2, 'othervalue')])
+
+        
+        
+        
+
+
+
+    def test_createLinkedList(self):
+        database = Database("test.db")
+
+        database.deleteFromUnsafe("tab")
+        database.createLinkedListIfNotExistsUnsafe("tab","col1")
+
+        tab1 = database.selectUnsafe("*", "tab")
+        self.assertEqual(tab1, [])
+
+        ins1 = database.insertLinkedListUnsafe("tab","col1","Joachim")
+        self.assertIsNotNone(ins1)
+        self.assertIsInstance(ins1, int)
+
+        app1 = database.appendLinkedListUnsafe("tab", "col1", "Janke", ins1)
+

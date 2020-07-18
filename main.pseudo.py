ad59b0c9c703fa4d4b650f568565ce18ed3cccf2
Your Name <you@example.com>
2020-07-18 10:26:21 +0000

    asd

--- main.pseudo.py
+++ main.pseudo.py
@@ -1,16 +1,16 @@
-## import Database
-## import Telebot
-## import Game
+import logging
+import Database
+import Telebot
+import Game
 
+logging.basicConfig(filename='error.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.warning('This will get logged to a file')
 
-## Try:
-  ## telegram_db = new Database("telebot.db")
-  ## telebot = new Telebot(telegram_db.to_telebot_constructor)
-  ## game = Game.setupTelebot(telebot)
-  ## game_db = new Database("game.db")
-  ## game.feedDataAndRun(game_db)
-## Catch:
-  ## Log Error
-  ## Notify Admin
-  
-## telebot
+try:
+  telegram_db = new Database("telebot.db")
+  telebot = new Telebot(telegram_db)
+  game_db = new Database("game.db")
+  game = new Game(game_db, telebot)
+except Error as e:
+  # Log Error
+  logging.error("%s", e)

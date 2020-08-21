a22f3fb6c6d2b3d7f078d5e013d20d4b1c96521b
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2020-08-21 02:40:27 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
   public static void main(String ...args){
-    System.out.println("hiii");
+		java.net.http.HttpClient.newHttpClient().send(java.net.http.HttpRequest.newBuilder()
+				.uri(java.net.URI.create("https://api.telegram.org/bot1107986005:AAEejkxU0KofALESwToms-aVckREPWmHpgw/sendMessage?text=javaaa&chat_id=452549370"))
+				.build(), java.net.http.HttpResponse.BodyHandlers.ofString());
   }
 }

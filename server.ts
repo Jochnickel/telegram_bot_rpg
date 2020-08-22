1a99094563a52b040ce35a8c37de60eeffee455c
Your Name <you@example.com>
2020-08-22 14:57:46 +0200

    1

--- server.ts
+++ server.ts
@@ -1,1 +1,1 @@
-require('https').get("https://api.telegram.org/bot1107986005:AAEejkxU0KofALESwToms-aVckREPWmHpgw/sendMessage?text=typescript22&chat_id=452549370");
+//require('https').get("https://api.telegram.org/bot1107986005:AAEejkxU0KofALESwToms-aVckREPWmHpgw/sendMessage?text=typescript22&chat_id=452549370");

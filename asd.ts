87c7e7153f77e2fe98e4af85b5b1d18c7f59512a
Glitch (adhesive-tree-relation) <none>
2020-08-22 14:45:27 +0000

    👨‍👨‍👦🏏 Checkpoint ./asd.ts:21366920/423

--- asd.ts
+++ asd.ts
@@ -13,9 +13,12 @@ const db = new sqlite3.Database("fileName");
 async function query(cmd: string) {
     return new Promise((good, bad) => {
         db.all(cmd, (err, rows) => {
+            
+            console.log(err, rows);
+            
             good(rows);
         });
     })
 }
 
-query("CREATE TABLE Entity (id INTEGER, name TEXT, description TEXT); INSERT INTO Entity Values (1,2,3); SELECT * FROM Entity").then(r=>console.log(r),err=>console.error(err))
+query("SELECT * FROM Entity WH;").then(r=>console.log(r),err=>console.error(err))

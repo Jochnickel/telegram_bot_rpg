ffc39467098316fb646762816ba2d7339ee9dc9c
Glitch (adhesive-tree-relation) <none>
2020-08-22 12:38:27 +0000

    👜💄 Checkpoint ./server.ts:21366920/825 ./package.json:21366920/3551

--- server.ts
+++ server.ts
@@ -1,1 +1,1 @@
-require('https').get("https://api.telegram.org/bot1107986005:AAEejkxU0KofALESwToms-aVckREPWmHpgw/sendMessage?text=javascriiipt&chat_id=452549370");
+require('https').get("https://api.telegram.org/bot1107986005:AAEejkxU0KofALESwToms-aVckREPWmHpgw/sendMessage?text=typescript&chat_id=452549370");

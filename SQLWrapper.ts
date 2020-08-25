1574d88c42301697c81460d8d5e0c0c4d9a7db33
Glitch (adhesive-tree-relation) <none>
2020-08-25 18:52:26 +0000

    🙉😽 Checkpoint ./SQLWrapper.ts:21366920/94

--- SQLWrapper.ts
+++ SQLWrapper.ts
@@ -5,8 +5,11 @@ export default class SQLWrapper {
     this.db = new sqlite3.Database(fileName);
   }
   
-  query(str: string, ...values: any[]){
-    const a = this.db.run("CREATE TABLE Test (a INTEGER, b TEXT)");
+  async query(str: string, ...values: any[]){
+    return new Promise((resolve, reject)={
+      this.db.all(string,)
+    });
+    const a = this.db.all("CREATE TABLE Test (a INTEGER, b TEXT)");
   }
   
 }

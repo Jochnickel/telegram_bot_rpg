8e17dd61f7cef23a84f88a59f995c0c2e65b7cd1
Your Name <you@example.com>
2020-09-14 13:46:08 +0000

    1

--- utils/SQLWrapper.ts
+++ utils/SQLWrapper.ts
@@ -5,7 +5,7 @@ import * as sqlite3 from "sqlite3";
 import * as AsyncLock from "async-lock";
 
 export default class SQL {
-	readonly #db;
+	readonly #db: sqlite3.Database;
 	readonly #fileName: string;
 	readonly #lock = new AsyncLock();
 
@@ -26,18 +26,21 @@ export default class SQL {
 	private async synchronized(foo: () => any) {
 		return this.#lock.acquire('key', foo);
 	}
-	
+
 	async query(cmd: string, ...values: any[]): Promise<any[]> {
 		return new Promise((resolve, reject) => {
 			try {
-				this.synchronized(this.#db.all(cmd, values, (err, rows) => {
-					if (err) {
-						console.error(err);
-						reject(err);
-					} else {
-						resolve(rows);
-					}
-				}));
+				const lockableFunction = () => {
+					this.#db.all(cmd, values, (err, rows) => {
+						if (err) {
+							console.error(err);
+							reject(err);
+						} else {
+							resolve(rows);
+						}
+					});
+				};
+				this.synchronized(lockableFunction);
 			} catch (error) {
 				console.error(error);
 				reject(error);
